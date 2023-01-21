b2c02916f25afb3b6346ed301d07f0e9563fe699
Abhinandan Wadhwa <awadhwa_be22@thapar.edu>
2023-01-22 01:38:54 +0530

    Landing Page Ready (font left)!

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import GetStarted from './pages/GetStarted';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="GetStarted" component={GetStarted} options={{
+          headerShown: false
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#BCCBDF',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ pages/GetStarted.jsx
@@ -0,0 +1,108 @@
+import { Button, Image, Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+// import {
+//   useFonts,
+//   MontserratAlternates_700Bold
+// } from "@expo-google-fonts/dev";
+
+const GetStarted = () => {
+  // let [fontsLoaded] = useFonts({
+  //   MontserratAlternates_700Bold
+  // });
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topPart}>
+        <Image source={require('../assets/homeImage.png')} style={styles.image} />
+      </View>
+      <View style={styles.bottomPart}>
+        <Text style={styles.headingP1}>Welcome to <Text style={styles.highlightedJoggie}>Joggie</Text>!</Text>
+        <Text style={styles.headingP2}>Let's make sport more fun!</Text>
+
+        <View style={styles.smallTextContainer}>
+          <Text style={styles.smallText}>This app will help increase your sport potential and</Text>
+          <Text style={styles.smallText}>find friends among like-minded people.</Text>
+        </View>
+        <Pressable style={styles.buttonContainer}>
+          <Text style={styles.buttonText}>Register</Text>
+        </Pressable>
+        <View style={styles.signInContainer}>
+          <Text style={styles.signInText1}>Already have an account?</Text>
+          <Text style={styles.signInText2}>Sign in</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default GetStarted;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#BCCBDF',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headingP1: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  headingP2: {
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  highlightedJoggie: {
+    color: '#5219DF',
+    fontWeight: 'bold',
+  },
+  bottomPart: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  topPart: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    width: 450,
+    height: 450,
+    marginTop: 50
+  },
+  smallTextContainer: {
+    marginTop: 10,
+    alignItems: 'center'
+  },
+  smallText: {
+    fontSize: 11,
+    letterSpacing: 1,
+    color: '#62748C'
+  },
+  buttonContainer: {
+    backgroundColor: '#5219DF',
+    width: 350,
+    marginTop: 50,
+    borderRadius: 6
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 18,
+    color: 'white',
+    letterSpacing: 0.8,
+    fontSize: 16
+  },
+  signInContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
+    alignItems: 'center'
+  },
+  signInText1: {
+
+  },
+  signInText2: {
+    marginLeft: 6,
+    fontWeight: 'bold'
+  },
+});
