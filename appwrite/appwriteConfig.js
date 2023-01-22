44f9b0cb8940253272996156838296935de52fb5
kennisjoseph <kennisjoseph5@gmail.com>
2023-01-22 12:42:10 +0530

    Sign in & SignUp Made still some work to do in appwrite!!

--- appwrite/appwriteConfig.js
+++ appwrite/appwriteConfig.js
@@ -1,11 +1,11 @@
 import { Client, Account, Databases } from 'appwrite';
 
+
+var dataId = "63cc26413d370c1bca1b";
 const client = new Client();
 
 client.setEndpoint("http://localhost/v1").setProject("63cc25a586dd7c7abacb")
-
 export const account = Account(client);
 
 //Database
-var dataId = "63cc26413d370c1bca1b";
-export const databases = new Databases(client, dataId)
+export const databases = new Databases(client, dataId);
